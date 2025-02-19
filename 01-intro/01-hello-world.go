9da1044182cac11bd3f990ede8a31d5de4750311
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-02-19 18:05:02 +0530

    introduction

---
+++ 01-intro/01-hello-world.go
@@ -0,0 +1,16 @@
+/* package declaration */
+package main
+
+/* import dependency packages */
+import "fmt"
+
+/* package scoped variable/ constant/ type declarations */
+/* package init function */
+/* main function */
+func main() {
+	// print("Hello, World!\n")
+	// fmt.Print("Hello, World!\n")
+	fmt.Println("Hello, World!")
+}
+
+/* other functions */
